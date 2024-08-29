4b85dbab0c87a589ac6a2b57106cad3256ef8894
Haci_of80 <Haci_of80@itstep.edu.az>
2024-08-29 13:49:55 +0400

    Add project files.

---
+++ ECommerce.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    //[Authorize(Roles="Admin,Editor")]//it is for all action methods
+    public class AdminController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Editor,Admin")]
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECommerce.WebUI/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿
+
+
+const inputElement = document.getElementById('searchBox');
+inputElement.addEventListener('keyDown', onKeyPress);
+
+function keyDown() {
+    var key = searchBox.value();
+    fetchData(key);
+}
+
+function fetchData(key) {
+    $.ajax({
+        url: '/Product/GetData',
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        data: { sKey: key },
+        success: function (response) {
+            $('#resultDiv').html(response);
+        },
+        error: function (xhr, status, error) {
+            console.log('Error: ' + error);
+        }
+    });
+}
